1abe88a45dad914f1bb3a89cd5cd25bf882a1c10
lcastilhog <lcastilho@flashvolve.com>
2024-06-14 12:05:07 -0300

    teste

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,6 +12,7 @@ defineProps({
     <h1 class="green">{{ msg }}</h1>
     <span>testando jenkins</span>
     <h3>
+      <span>testando jenkins</span>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
